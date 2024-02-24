f4c6e963f6b52bc9d4ea87356762ae1bd9699b01
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-02-24 13:20:21 +0800

    documentation

--- src/structures/mod.rs
+++ src/structures/mod.rs
@@ -2,12 +2,21 @@ use rand::random;
 
 pub mod models;
 pub mod websocket;
+
+
+/// # rand
+/// randomly generates u32 numbers and turns into strings, used for IDs
+pub fn rand() -> String {
+    random::<u32>().to_string()
+}
+
+/// # is_false
+/// a simple check used by serde
 pub fn is_false(i: &bool) -> bool {
     !*i
 }
+/// # is_none
+/// a simple check used by serde
 pub fn is_none<T>(i: &Option<T>) -> bool {
     i.is_none()
-}
-pub fn rand() -> String {
-    random::<u32>().to_string()
 }
