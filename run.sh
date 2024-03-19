e3c6192a37865c69bc043950f9ceef1e547448ea
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-03-19 13:04:18 +0800

    moved more functions to update.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 # overriding old log file
 echo "" > backend_logs
-
-# deleted modified local cache
-#
-# due to issues with file ownership this must be performed
-git checkout . >> backend_logs
-# compile
-mold --run cargo b -r >> backend_logs
 # run program
 nohup cargo r -r >> backend_logs &
--- update.sh
+++ update.sh
@@ -1,3 +1,10 @@
 # update software
 git pull
 cargo update
+
+# deleted modified local cache
+#
+# due to issues with file ownership this must be performed
+git checkout . >> backend_logs
+# compile
+mold --run cargo b -r >> backend_logs
