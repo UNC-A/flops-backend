ca0c015e2d46f82e12f7577a91ea3a1dc426933d
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-03-19 12:51:25 +0800

    improved log coverage to include git and compilation

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
+# overriding old log file
+echo "" > backend_logs
+
 # deleted modified local cache
 #
 # due to issues with file ownership this must be performed
-git checkout .
+git checkout . >> backend_logs
 # compile
-mold --run cargo b -r
+mold --run cargo b -r >> backend_logs
 # run program
-nohup cargo r -r > backend_logs &
+nohup cargo r -r >> backend_logs &
