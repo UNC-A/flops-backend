9658437b908fddb41cd90d75991fdad2fb87165d
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-03-19 13:05:09 +0800

    reordered git commands

--- update.sh
+++ update.sh
@@ -1,10 +1,12 @@
-# update software
-git pull
-cargo update
-
 # deleted modified local cache
 #
 # due to issues with file ownership this must be performed
 git checkout . >> backend_logs
+
+# update software
+git pull
+cargo update
+
+
 # compile
 mold --run cargo b -r >> backend_logs
